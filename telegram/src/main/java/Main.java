fe0f05613e5a992683cedcf74ace6072845f0922
edechambrier <37554761+edechambrier@users.noreply.github.com>
2019-12-16 16:29:57 +0100

    mise en place

---
+++ telegram/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        TelegramBot bot = new TelegramBot("930161348:AAE6K8Uy61B9AWgOlY7EB22po4KhCoKCQiI");
+    }
+}
