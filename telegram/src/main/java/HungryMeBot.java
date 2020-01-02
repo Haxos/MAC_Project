a301c8581dba6c1837312695ecea8e9f8cc74d04
edechambrier <37554761+edechambrier@users.noreply.github.com>
2020-01-02 23:43:05 +0100

    Auto reply bot working

---
+++ telegram/src/main/java/HungryMeBot.java
@@ -0,0 +1,29 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class HungryMeBot  extends TelegramLongPollingBot {
+
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getBotUsername() {
+        return "HungryMe_bot";
+    }
+
+    public String getBotToken() {
+        return "930161348:AAE6K8Uy61B9AWgOlY7EB22po4KhCoKCQiI";
+    }
+}
