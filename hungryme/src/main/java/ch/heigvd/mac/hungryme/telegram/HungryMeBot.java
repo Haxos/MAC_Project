e9251bc4d45a6052e8a6a2f7e4a4e571c6c2363a
edechambrier <37554761+edechambrier@users.noreply.github.com>
2020-01-12 17:09:19 +0100

    buttons testing

--- hungryme/src/main/java/ch/heigvd/mac/hungryme/telegram/HungryMeBot.java
+++ hungryme/src/main/java/ch/heigvd/mac/hungryme/telegram/HungryMeBot.java
@@ -11,7 +11,12 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.ParseMode;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HungryMeBot extends TelegramLongPollingBot {
 
@@ -41,6 +46,33 @@ public class HungryMeBot extends TelegramLongPollingBot {
             message.setChatId(update.getMessage().getChatId());
             message.setParseMode(ParseMode.HTML);
             message.setText(recipe.toString());
+
+
+
+            // Create ReplyKeyboardMarkup object
+            ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+            // Create the keyboard (list of keyboard rows)
+            List<KeyboardRow> keyboard = new ArrayList<>();
+            // Create a keyboard row
+            KeyboardRow row = new KeyboardRow();
+            // Set each button, you can also use KeyboardButton objects if you need something else than text
+            row.add("Row 1 Button 1");
+            row.add("Row 1 Button 2");
+            row.add("Row 1 Button 3");
+            // Add the first row to the keyboard
+            keyboard.add(row);
+            // Create another keyboard row
+            row = new KeyboardRow();
+            // Set each button for the second line
+            row.add("Row 2 Button 1");
+            row.add("Row 2 Button 2");
+            row.add("Row 2 Button 3");
+            // Add the second row to the keyboard
+            keyboard.add(row);
+            // Set the keyboard to the markup
+            keyboardMarkup.setKeyboard(keyboard);
+            // Add it to the message
+            message.setReplyMarkup(keyboardMarkup);
 
 
             /*SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
