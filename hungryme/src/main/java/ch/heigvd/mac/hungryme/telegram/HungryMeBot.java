46d528ba98362af177474de8016bc2d49ccaae5a
edechambrier <37554761+edechambrier@users.noreply.github.com>
2020-01-17 22:52:40 +0100

    add recipe status to message

--- hungryme/src/main/java/ch/heigvd/mac/hungryme/telegram/HungryMeBot.java
+++ hungryme/src/main/java/ch/heigvd/mac/hungryme/telegram/HungryMeBot.java
@@ -26,6 +26,8 @@ import java.util.List;
 
 import java.lang.Math;
 
+import static java.lang.Math.toIntExact;
+
 public class HungryMeBot extends TelegramLongPollingBot {
 
     final String like = "\uD83D\uDC4D";
@@ -60,7 +62,7 @@ public class HungryMeBot extends TelegramLongPollingBot {
 
 
             // tockenize the message
-            String tockens[]= update.getMessage().getText().split("\\s+");
+            String tockens[]= update.getMessage().getText().toLowerCase().split("\\s+");
 
 
             // parse the message and get the replies
@@ -75,40 +77,6 @@ public class HungryMeBot extends TelegramLongPollingBot {
                 }
             }
 
-            /*
-            InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
-            List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
-            List<InlineKeyboardButton> rowInline = new ArrayList<>();
-            rowInline.add(new InlineKeyboardButton().setText("Update message text").setCallbackData("IDIDIDIDIDDIDIDID"));
-            // Set the keyboard to the markup
-            rowsInline.add(rowInline);
-            // Add it to the message
-            markupInline.setKeyboard(rowsInline);
-            message.setReplyMarkup(markupInline);
-
-
-            // Create ReplyKeyboardMarkup object
-            ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-            // Create the keyboard (list of keyboard rows)
-            List<KeyboardRow> keyboard = new ArrayList<>();
-            // Create a keyboard row
-            KeyboardRow row = new KeyboardRow();
-            // Set each button, you can also use KeyboardButton objects if you need something else than text
-            row.add("\uD83D\uDC4D like");
-            row.add("\uD83D\uDC4E dislike");
-            row.add("❤️ add to favorite");
-            row.add("Show more");
-            // Add the first row to the keyboard
-            keyboard.add(row);
-            keyboardMarkup.setKeyboard(keyboard);
-            // Add it to the message
-            message.setReplyMarkup(keyboardMarkup);
-
-            try {
-                execute(message); // Call method to send the message
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }*/
         }else if (update.hasCallbackQuery()) {
             // Set variables
             String call_data[]= update.getCallbackQuery().getData().split("\\s+");
@@ -117,29 +85,36 @@ public class HungryMeBot extends TelegramLongPollingBot {
             long message_id = update.getCallbackQuery().getMessage().getMessageId();
             long chat_id = update.getCallbackQuery().getMessage().getChatId();
 
-
-            /*
-            //Recipe recipe = _documentDB.getRecipeById("5e1c74cefd1a3f219f977b2a");
-
-            */
             if(call_data[0].equals(like)){
                 if(_graphDB.isRecipeDisliked(call_data[1], userId)){
                     _graphDB.unDislikeRecipe(call_data[1], userId);
                 }else {
                     _graphDB.likeRecipe(call_data[1], userId);
                 }
+
+                // replace the original message
+                replaceMessage(call_data[1], userId, chat_id, message_id);
+
             }else if(call_data[0].equals(dislike)){
                 if(_graphDB.isRecipeLiked(call_data[1], userId)){
                     _graphDB.unLikeRecipe(call_data[1], userId);
                 }else{
                     _graphDB.dislikeRecipe(call_data[1], userId);
                 }
+                // replace the original message
+                replaceMessage(call_data[1], userId, chat_id, message_id);
             }else if(call_data[0].equals(addFav)){
                 _graphDB.favoriteRecipe(call_data[1], userId);
+                // replace the original message
+                replaceMessage(call_data[1], userId, chat_id, message_id);
             }else if(call_data[0].equals(remFav)){
                 _graphDB.unFavoriteRecipe(call_data[1], userId);
+                // replace the original message
+                replaceMessage(call_data[1], userId, chat_id, message_id);
             }else if (call_data[0].length() == "5e1c74cefd1a3f219f977b2a".length() && call_data.length == 1) {
                 String answer = format(_documentDB.getRecipeById(call_data[0]));
+                answer = addFooterInfo(answer, call_data[0] ,userId);
+
                 SendMessage new_message = new SendMessage()
                         .setChatId(chat_id)
                         .setText(answer)
@@ -184,7 +159,7 @@ public class HungryMeBot extends TelegramLongPollingBot {
 
                 for(int i = 0; i< call_data.length; i++){
                     List<InlineKeyboardButton> rowInline = new ArrayList<>();
-                    rowInline.add(new InlineKeyboardButton().setText(call_data[i]).setCallbackData(call_data[i]));
+                    rowInline.add(new InlineKeyboardButton().setText(getRecipeName(call_data[i])).setCallbackData(call_data[i]));
                     rowsInline.add(rowInline);
                 }
 
@@ -256,6 +231,10 @@ public class HungryMeBot extends TelegramLongPollingBot {
         return telegramRecipe;
     }
 
+    private String getRecipeName(String recipeId){
+       return _documentDB.getRecipeById(recipeId).getName();
+    }
+
     //private List<String> parseResult(String[] tokens){
     private ArrayList<SendMessage> parseResult(String[] tokens, Long chatId){
         // keywords
@@ -273,16 +252,7 @@ public class HungryMeBot extends TelegramLongPollingBot {
 
         if(tokens[0].equals(unique)){  //unique recipe
             System.out.println("==> unique recipe");
-        }else if(tokens[0].equals(like)){ // Like
-            System.out.println("==> like");
-        }else if(tokens[0].equals(dislike)){ // dislike
-            System.out.println("==> dislike");
-        }else if(tokens[0].equals(addFav)){   // add to favorite
-            System.out.println("==> addd to favorite");
-        }else if(tokens[0].equals(remFav)){// remove from favorite
-            System.out.println("==> remove from favorite");
         }
-
         //a {time : (very) fast/short} recipe(s) with {ingredients} for {tags}
 
         // get ingredients
@@ -311,13 +281,6 @@ public class HungryMeBot extends TelegramLongPollingBot {
             InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
             List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
 
-            // add all recipe buttons
-            /*for(ArrayList<String> recipe : recipesId){
-                List<InlineKeyboardButton> rowInline = new ArrayList<>();
-                rowInline.add(new InlineKeyboardButton().setText(recipe.get(1)).setCallbackData(recipe.get(0)));
-                rowsInline.add(rowInline);
-            }*/
-
             for(int i = 0; i<5 && i<recipesId.size(); i++){
                 ArrayList<String> recipe = recipesId.pollFirst();
                 List<InlineKeyboardButton> rowInline = new ArrayList<>();
@@ -327,14 +290,16 @@ public class HungryMeBot extends TelegramLongPollingBot {
 
             if(recipesId.size() > 0) {
                 String remainingRecipeId = "";
+
                 for(int i = 0; i< recipesId.size() && remainingRecipeId.length() <= 40; i++){
                     remainingRecipeId = remainingRecipeId.concat(recipesId.get(i).get(0)+" ");
                 }
-
-                System.out.println(remainingRecipeId);
-
                 List<InlineKeyboardButton> rowInline = new ArrayList<>();
-                rowInline.add(new InlineKeyboardButton().setText("➕ Show more ! ➕").setCallbackData(remainingRecipeId));
+                if(recipesId.size() == 1) {
+                    rowInline.add(new InlineKeyboardButton().setText(recipesId.get(0).get(1)).setCallbackData(remainingRecipeId));
+                }else{
+                    rowInline.add(new InlineKeyboardButton().setText("➕ Show more ! ➕").setCallbackData(remainingRecipeId));
+                }
                 rowsInline.add(rowInline);
             }
 
@@ -344,6 +309,66 @@ public class HungryMeBot extends TelegramLongPollingBot {
             messages.add(message);
         }
 
+
+
         return messages;
+    }
+
+    private String addFooterInfo(String recipe, String recipeId, String userId){
+
+        if(_graphDB.isRecipeLiked(recipeId, userId) ||
+                _graphDB.isRecipeDisliked(recipeId, userId) ||
+                _graphDB.isRecipeFavorite(recipeId, userId)){
+            recipe = recipe.concat("\n");
+            if(_graphDB.isRecipeLiked(recipeId, userId))
+                recipe = recipe.concat(like);
+            if(_graphDB.isRecipeDisliked(recipeId, userId))
+                recipe = recipe.concat(dislike);
+            if(_graphDB.isRecipeFavorite(recipeId, userId))
+                recipe = recipe.concat(addFav);
+        }
+
+        return recipe;
+    }
+
+    private void replaceMessage(String recipeId, String userId, Long chatId, Long messageId){
+        // replace the original message
+        String answer = format(_documentDB.getRecipeById(recipeId));
+        answer = addFooterInfo(answer, recipeId, userId);
+
+        EditMessageText new_message = new EditMessageText()
+                .setChatId(chatId)
+                .setMessageId(toIntExact(messageId))
+                .setText(answer)
+                .setParseMode(ParseMode.HTML);
+
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+
+        // add all recipe buttons
+
+        List<InlineKeyboardButton> rowInline = new ArrayList<>();
+        if(!_graphDB.isRecipeLiked(recipeId, userId))
+            rowInline.add(new InlineKeyboardButton().setText(like).setCallbackData(like+" "+ recipeId));
+
+        if(!_graphDB.isRecipeDisliked(recipeId, userId))
+            rowInline.add(new InlineKeyboardButton().setText(dislike).setCallbackData(dislike+" "+ recipeId));
+
+        if(!_graphDB.isRecipeFavorite(recipeId, userId)) {
+            rowInline.add(new InlineKeyboardButton().setText(addFav).setCallbackData(addFav + " " + recipeId));
+        }else{
+            rowInline.add(new InlineKeyboardButton().setText(remFav).setCallbackData(remFav + " " + recipeId));
+        }
+        rowsInline.add(rowInline);
+
+        // Add it to the message
+        markupInline.setKeyboard(rowsInline);
+        new_message.setReplyMarkup(markupInline);
+
+        try {
+            execute(new_message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
